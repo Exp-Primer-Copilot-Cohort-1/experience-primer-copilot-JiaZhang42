66694ce10575c12e3bef6a140873b11c6daddf52
Jia Zhang <20775804+JiaZhang42@users.noreply.github.com>
2024-01-29 13:06:39 +0000

    copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,6 @@
-function skillMember()) {
-    console.log('skill member')
-}
+function skillMember() {
+    var member = document.getElementById('member');
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberList = document.getElementById('memberList');
+    var memberListValue = memberList.options[memberLi
